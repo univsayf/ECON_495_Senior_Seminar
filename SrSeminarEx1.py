4c64421e8d9c04293a35d6b8dd00c1c637a5b0e4
ATM Sayfuddin <42285000+univsayf@users.noreply.github.com>
2019-10-18 12:44:53 -0400

    Class Exercise 1 uploaded

---
+++ SrSeminarEx1.py
@@ -0,0 +1,33 @@
+
+# Question 1: Create a list using the variable below (follow the same order as below). Name list "new_list."
+x = 1
+y = 0
+z = 2
+
+# Question 2: Import the package called "numpy"
+
+# Create a variable named "variable1" that produces a numpy array as follows: [2 4 6]
+
+# Create a variable named "variable2" that produces a numpy array as follows: [2 True False]
+
+# Complete the following line of codes to produce an output as follows: [4 5 6]
+
+print(variable1 ? variable2)
+
+# Work with "pandas" lybrary. Below I am importing pandas library 
+import pandas as pd
+
+# Use the following link to download a dataset: https://github.com/univsayf/ECON_495_Senior_Seminar/blob/master/wage_data.csv.zip
+
+# Below I have provoded the codes to load the data set. Run the code.
+df = pd.read_csv('wage_data.csv') 
+
+# the following code gives a list of columns available in a dataset
+list(df)
+
+# You can extract a column of a dataframe/dataset by using the following command: df.column_name
+# Your task is use functions of numpy arrays to give the following statistics: mean, standard deviation, minimum value, maximum value, and median.
+
+
+
+
