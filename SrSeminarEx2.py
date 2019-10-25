62e3a828fc43bb8a33480df0f9d640f12e629559
ATM Sayfuddin <42285000+univsayf@users.noreply.github.com>
2019-10-25 12:54:26 -0400

    Added Second Class Exercise

---
+++ SrSeminarEx2.py
@@ -0,0 +1,38 @@
+# Create a function for each of the four questions below: 
+
+""" Question 1:
+	# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+	# Examples: 
+		string_times('Hi', 2) → 'HiHi'
+		string_times('Hi', 3) → 'HiHiHi'
+		string_times('Hi', 1) → 'Hi'
+"""
+
+""" Question 2: Given an array of integers, return True if 6 appears as either 
+	the first or last element in the array. The array will be length 1 or more.
+	Examples: 
+		first_last6([1, 2, 6]) → True
+		first_last6([6, 1, 2, 3]) → True
+		first_last6([13, 6, 1, 2, 3]) → False
+"""
+
+""" Question 3: Given an array of ints, return True if the array is length 1 or 
+	more, and the first element and the last element are equal.
+	Examples:
+		same_first_last([1, 2, 3]) → False
+		same_first_last([1, 2, 3, 1]) → True
+		same_first_last([1, 2, 1]) → True
+"""
+
+""" Questioin 4: When squirrels get together for a party, they like to have cigars. 
+	A squirrel party is successful when the number of cigars is between 40 and 60, 
+	inclusive. Unless it is the weekend, in which case there is no upper bound on 
+	the number of cigars. Return True if the party with the given values is successful, 
+	or False otherwise.
+	Examples: 
+	cigar_party(30, False) → False
+	cigar_party(50, False) → True
+	cigar_party(70, True) → True
+"""
+
+
